b10a95e2f838687db2b7feb7ea09c37f68891214
MoisesVinajera <moisesvinajera@gmail.com>
2022-06-05 23:20:05 -0500

    Add Exercise_1.8 and 1.9

--- Part1/unicafe/src/container/Statistics.jsx
+++ Part1/unicafe/src/container/Statistics.jsx
@@ -11,19 +11,28 @@ const Statistics = ({ goodValue, neutralValue, badValue, allValue }) => {
 
   const calculatePositiveAverage = () => {
     if (allValue > 0) {
-      return `${goodValue / allValue} %`;
+      return `${(goodValue / allValue) * 100} %`;
     }
   };
 
   return (
     <>
       <Title title="Statistics" />
-      <ElementCollected text="Good" value={goodValue} />
-      <ElementCollected text="Neutral" value={neutralValue} />
-      <ElementCollected text="Bad" value={badValue} />
-      <ElementCollected text="All" value={allValue} />
-      <ElementCollected text="Average" value={calculateAverage()} />
-      <ElementCollected text="Positive" value={calculatePositiveAverage()} />
+      {allValue > 0 ? (
+        <>
+          <ElementCollected text="Good" value={goodValue} />
+          <ElementCollected text="Neutral" value={neutralValue} />
+          <ElementCollected text="Bad" value={badValue} />
+          <ElementCollected text="All" value={allValue} />
+          <ElementCollected text="Average" value={calculateAverage()} />
+          <ElementCollected
+            text="Positive"
+            value={calculatePositiveAverage()}
+          />
+        </>
+      ) : (
+        <p>No given feedback</p>
+      )}
     </>
   );
 };
